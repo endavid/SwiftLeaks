95284db1e8b429738f738c48fd103b441fa83ba5
David Gavilan <david.gavilan.ruiz@gmail.com>
2017-03-04 22:10:07 +0000

    Remove stuff — still leaks

--- SwiftLeaks/Primitive.swift
+++ SwiftLeaks/Primitive.swift
@@ -11,99 +11,6 @@ import Metal
 import MetalKit
 import simd
 
-// linear RGB with alpha
-struct LinearRGBA {
-    let rgba : float4
-    
-    var r : Float {
-        get {
-            return rgba.x
-        }
-    }
-    var g : Float {
-        get {
-            return rgba.y
-        }
-    }
-    var b : Float {
-        get {
-            return rgba.z
-        }
-    }
-    var a : Float {
-        get {
-            return rgba.w
-        }
-    }
-    
-    init(r: Float, g: Float, b: Float, a: Float) {
-        rgba = float4(r, g, b, a)
-    }
-    
-    init(rgb: float3) {
-        rgba = float4(rgb.x, rgb.y, rgb.z, 1.0)
-    }
-    
-    init(srgba: NormalizedSRGBA) {
-        let f = {(c: Float) -> Float in
-            if c <= 0.04045 {
-                return c / 12.92
-            }
-            return powf((c + 0.055) / 1.055, 2.4)
-        }
-        rgba = float4(f(srgba.r), f(srgba.g), f(srgba.b), srgba.a)
-    }
-    
-    init(_ color: UIColor) {
-        var fRed : CGFloat = 0
-        var fGreen : CGFloat = 0
-        var fBlue : CGFloat = 0
-        var fAlpha : CGFloat = 0
-        color.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha)
-        rgba = float4(Float(fRed), Float(fGreen), Float(fBlue), Float(fAlpha))
-    }
-}
-
-struct NormalizedSRGBA {
-    let rgba : float4
-    
-    var r : Float {
-        get {
-            return rgba.x
-        }
-    }
-    var g : Float {
-        get {
-            return rgba.y
-        }
-    }
-    var b : Float {
-        get {
-            return rgba.z
-        }
-    }
-    var a : Float {
-        get {
-            return rgba.w
-        }
-    }
-    
-    init(r: Float, g: Float, b: Float, a: Float) {
-        rgba = float4(r, g, b, a)
-    }
-    
-    init(rgba: LinearRGBA) {
-        let f = {(c: Float) -> Float in
-            if c <= 0.0031308 {
-                return c * 12.92
-            }
-            return powf(c * 1.055, 1/2.4) - 0.055
-        }
-        self.rgba = float4(f(rgba.r), f(rgba.g), f(rgba.b), rgba.a)
-    }
-    
-}
-
 struct Vec2 {
     let x : Float
     let y : Float
@@ -114,11 +21,9 @@ struct Vec2 {
 }
 struct Material {
     static let white = Material(
-        diffuse: LinearRGBA(rgb: float3(1,1,1)),
         uvScale: Vec2(1,1),
         uvOffset: Vec2(0,0)
     )
-    var diffuse : LinearRGBA
     var uvScale : Vec2
     var uvOffset : Vec2
 }
@@ -139,22 +44,13 @@ enum LightingType {
 
 class Primitive {
     internal var vertexBuffer : MTLBuffer!
-    let priority : Int
     var name: String = ""
-    var perInstanceUniforms : [PerInstanceUniforms]
     let uniformBuffer : MTLBuffer!
     var lightingType: LightingType = .LitOpaque
     var submeshes: [Mesh] = []
     var mysub : [MyTest] = []
     
-    var numInstances : Int {
-        get {
-            return perInstanceUniforms.count
-        }
-    }
-    init(priority: Int, numInstances: Int) {
-        self.priority = priority
-        self.perInstanceUniforms = [PerInstanceUniforms](repeating: PerInstanceUniforms(transform: Transform(), material: Material.white), count: numInstances)
+    init() {
         self.uniformBuffer = nil// RenderManager.sharedInstance.createPerInstanceUniformsBuffer("primUniforms", numElements: RenderManager.NumSyncBuffers * numInstances)
     }
     struct Mesh {
@@ -176,23 +72,9 @@ class Primitive {
     }
 }
 
-struct SphereDescriptor {
-    let isRegularGrid: Bool
-    let isInterior: Bool
-    let tessellationLevel: Int // only for non-regular grids
-    let widthSegments: Int // only for regular grids
-    let heightSegments: Int // only for regular grids
-}
-
 class SpherePrimitive : Primitive {
-    // this could probably be a series of flags, hasInterior & hasExterior
-    fileprivate let isInterior : Bool
-    
-    
-    /// @param tesselationLevel: 2: 162 vertices; 3: 642 vertices; 4: 2562 vertices
-    init(priority: Int, numInstances: Int, descriptor: SphereDescriptor) {
-        self.isInterior = descriptor.isInterior
-        super.init(priority: priority, numInstances: numInstances)
+    override init() {
+        super.init()
         mysub.append(MyTest(someNumber: 80))
     }
 }
@@ -207,23 +89,8 @@ class MyScene : Scene {
         setup()
     }
     func setup() {
-        let sphereDesc = SphereDescriptor(isRegularGrid: false, isInterior: false, tessellationLevel: 2, widthSegments: 0, heightSegments: 0)
-        let spheres = SpherePrimitive(priority: 0, numInstances: 1, descriptor: sphereDesc)
+        let spheres = SpherePrimitive()
         primitives.append(spheres)
     }
 }
 
-class Plugin {
-    var primitives : [Primitive] = []
-    func queue(_ primitive: Primitive) {
-        primitives.append(primitive)
-    }
-}
-
-class Manager {
-    static let sharedInstance = Manager()
-    var plugins : [Plugin] = []
-    func initPlugins() {
-        plugins.append(Plugin())
-    }
-}
--- SwiftLeaks/ViewController.swift
+++ SwiftLeaks/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        Manager.sharedInstance.initPlugins()
         scene = MyScene()
     }
 
